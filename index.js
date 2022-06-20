aa6ae076a77e7f799987fb2bef200e49a41e3055
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-06-19 21:39:51 -0700

    added code to create the readme file after inputting the title

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: 'Title?',
+        name: 'title',
+        type: 'input',
+    }
+];
+
+inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        fs.writeFile(`${answers.title}.md`, JSON.stringify(answers, null , 2), err => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log('success');
+          })
+        })
+
+
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
