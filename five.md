fd3e88abc77c5062eaef736ea5c8195ea6306388
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-06-20 21:18:21 -0700

    added the questions

---
+++ five.md
@@ -0,0 +1,12 @@
+{
+  "title": "five",
+  "description": "A num",
+  "installation": "jus do it",
+  "usage": "use it",
+  "contribution": "nah",
+  "test": "yahhhh....nah",
+  "liscense": [
+    ""
+  ],
+  "contact": "email.com"
+}
--- index.js
+++ index.js
@@ -1,14 +1,62 @@
+
+
+
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    // title
     {
         message: 'Title?',
         name: 'title',
         type: 'input',
-    }
+    },
+    // description
+    {
+        message: 'Description?',
+        name: 'description',
+        type: 'input',
+    },
+    // installation instructions
+    {
+        message: 'Installation Instructions?',
+        name: 'installation',
+        type: 'input',
+    },
+    // usage info
+    {
+        message: 'Usage info?',
+        name: 'usage',
+        type: 'input',
+    },
+    // contribution guidelines
+    {
+        message: 'Contribution guidelines?',
+        name: 'contribution',
+        type: 'input',
+    },
+    // test instructions
+    {
+        message: 'Test instructions?',
+        name: 'test',
+        type: 'input',
+    },
+    // liscense
+    {
+        message: 'Liscense?',
+        name: 'liscense',
+        type: 'checkbox',
+        choices: ['', ''],
+    },
+    // contact info
+    {
+        message: 'Contact info/email?',
+        name: 'contact',
+        type: 'input',
+    },
 ];
 
 inquirer
