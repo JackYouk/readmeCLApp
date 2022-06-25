0370b8077ffe075525a2cea047d27bc4a9de7b17
JackYouk <jackyoukstetter1@gmail.com>
2022-06-24 19:06:52 -0700

    made app functional. ran some tests. need to get images working and maybe smaller if i can.

--- index.js
+++ index.js
@@ -76,10 +76,30 @@ inquirer
             usage, 
             contribution,
             test,
-            liscense,
+            license,
             github,
             email,
         } = answers;
+
+        // license logic
+        let licenseInfo = '';
+        let licenseImg = '';
+        switch(license[0]){
+          // MIT
+          case 'MIT':
+            licenseImg = '![alt text](https://imgs.search.brave.com/wnHtjB3tdNVQuGc6JO-6pjSJllStWbawxWCzmVwzScU/rs:fit:300:300:1/g:ce/aHR0cHM6Ly93d3cu/aW5jbHVkZWhlbHAu/Y29tL2Zvc3MvSW1h/Z2VzL21pdC5qcGc)';
+            licenseInfo = 'Limited restriction on reuse, distribution of closed source versions is allowed.';
+            break;
+          // GNU GPLv3
+          case 'GNU GPLv3':
+            licenseImg = '![alt text](https://imgs.search.brave.com/kqoZBsdwI3iST_3mzEJxqo1EQvIQJ8tFcZ3GHpz7AZ0/rs:fit:280:216:1/g:ce/aHR0cHM6Ly93d3cu/YXBlcnR1cy5vcmcv/c2l0ZXMvZGVmYXVs/dC9maWxlcy9zdHls/ZXMvdGh1bWJuYWls/L3B1YmxpYy9pbWFn/ZXMvR1BMdjMtbGFy/Z2UucG5nP2l0b2s9/bjNxSXRSQnc)';
+            licenseInfo = 'Limited restriction on reuse, distribution of closed source versions is restricted.';
+            break;
+          // no license
+          default:
+            licenseInfo = 'Code is entirely open-source, protected by no licenses';
+            break;
+        }
 
 
         // initiliazes readme
@@ -90,9 +110,18 @@ inquirer
             }
             console.log('success');
           })
+        
+        // adds license image towards the top
+        fs.appendFileSync(`${title}.md`, `${licenseImg}\n`, err => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('success');
+        })
 
         // Appends description section
-        fs.appendFile(`${title}.md`, `##Description\n ${description}\n`, err => {
+        fs.appendFileSync(`${title}.md`, `###Description\n ${description}\n`, err => {
             if (err) {
               console.log(err);
               return;
@@ -100,7 +129,7 @@ inquirer
             console.log('success');
           })
         // Appends installation section
-        fs.appendFile(`${title}.md`, `##Installation\n ${installation}\n`, err => {
+        fs.appendFileSync(`${title}.md`, `###Installation\n ${installation}\n`, err => {
             if (err) {
               console.log(err);
               return;
@@ -108,7 +137,7 @@ inquirer
             console.log('success');
           })
         // Appends usage section
-        fs.appendFile(`${title}.md`, `##Usage\n ${usage}\n`, err => {
+        fs.appendFileSync(`${title}.md`, `###Usage\n ${usage}\n`, err => {
             if (err) {
               console.log(err);
               return;
@@ -116,7 +145,7 @@ inquirer
             console.log('success');
           })
         // Appends contribution guidelines section
-        fs.appendFile(`${title}.md`, `##Contribution Guidelines\n ${contribution}\n`, err => {
+        fs.appendFileSync(`${title}.md`, `###Contribution Guidelines\n ${contribution}\n`, err => {
             if (err) {
               console.log(err);
               return;
@@ -124,7 +153,7 @@ inquirer
             console.log('success');
           })
         // Appends test instructions section
-        fs.appendFile(`${title}.md`, `##Test Instructions\n ${test}\n`, err => {
+        fs.appendFileSync(`${title}.md`, `###Test Instructions\n ${test}\n`, err => {
             if (err) {
               console.log(err);
               return;
@@ -132,40 +161,22 @@ inquirer
             console.log('success');
           })
         // Appends License section
-        switch(liscense){
-          // MIT
-          case 'MIT':
-            fs.appendFile(`${title}.md`, `##License\n MIT - Limited restriction on reuse, distribution of closed source versions is allowed.\n`, err => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              console.log('success');
-            })
-            break;
-          // GNU GPLv3
-          case 'GNU GPLv3':
-            fs.appendFile(`${title}.md`, `##License\n GNU GPLv3 - Limited restriction on reuse, distribution of closed source versions is allowed.\n`, err => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              console.log('success');
-            })
-            break;
-          // no license
-          default:
-            fs.appendFile(`${title}.md`, `##License\n GNU GPLv3 - Limited restriction on reuse, distribution of closed source versions is allowed.\n`, err => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              console.log('success');
-            })
-            break;
-        }
+        fs.appendFileSync(`${title}.md`, `###License\n This code is protected by ${license}. This implies: ${licenseInfo}\n`, err => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('success');
+        })
         
         // Appends contact section
           // github profile
           // email
+          fs.appendFileSync(`${title}.md`, `###Contact Info\n Github: ${github} & Email: ${email}`, err => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log('success');
+          })
     })
